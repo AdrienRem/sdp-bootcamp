f380bc04fb03336cbbfa5610daf80b4b3bc9c747
Adrien Remillieux <adrien.remillieux@epfl.ch>
2020-02-27 16:25:49 +0100

    completed point d

--- app/src/main/java/ch/epfl/sdp/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/MainActivity.java
@@ -1,8 +1,10 @@
 package ch.epfl.sdp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setGreeting(View view) {
+        TextView display = findViewById(R.id.textView);
+        display.setText("button clicked");
     }
 }
